a171e48d8626927b5d8c37c6c30ff5388606fda7
SinaHaas <44026421+SinaHaas@users.noreply.github.com>
2019-04-17 22:47:35 +0200

    Konzept, BerechneGesamtpreis, Anzeigen Auswahl, Bilder

--- aufgabe4/js/main.js
+++ aufgabe4/js/main.js
@@ -5,6 +5,7 @@ Matrikel: <260918>
 Datum: <18.04.2019>
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
+let Bestellung = [];
 var Eisdealer;
 (function (Eisdealer) {
     window.addEventListener("load", init);
@@ -14,6 +15,7 @@ var Eisdealer;
         for (let i = 0; i < fieldsets.length; i++) {
             let fieldset = fieldsets[i];
             fieldset.addEventListener("change", bestellen);
+            fieldset.addEventListener("change", berechnePreis);
         }
     }
     function bestellen(_event) {
@@ -23,10 +25,34 @@ var Eisdealer;
             console.log("Changed " + target.name + " to " + target.checked);
         let prodElement = document.createElement('div');
         document.getElementById("bestellung").appendChild(prodElement);
+        Bestellung.push(target.value);
         let gewählt = `
-            <p id="${target.id}">${target.id}</p>
-            `;
+            <p id="${target.id}">${Bestellung}</p>`;
         prodElement.innerHTML = gewählt;
     }
 })(Eisdealer || (Eisdealer = {}));
+let Eis = [];
+let Zusatz = [];
+function berechnePreis(_event) {
+    let currentEventtarget = _event.currentTarget;
+    let target = _event.target;
+    if (currentEventtarget.id == "eissorten") {
+        /*alert("HiEissorte");*/
+        Eis.push(target.value);
+        console.log(Eis.length * 1);
+    }
+    else if (currentEventtarget.id == "zusätze") {
+        /*alert("hizusatz")*/
+        Zusatz.push(target.value);
+        console.log(Zusatz.length * 0.5);
+    }
+    ;
+    let Ergebnis = Eis.length * 1;
+    let Ergebnis2 = Zusatz.length * 0.5;
+    let prodElement = document.createElement('div');
+    document.getElementById("bestellung").appendChild(prodElement);
+    let ergebnis = `
+            <p id="${target.id}">${Ergebnis + Ergebnis2}</p>`;
+    prodElement.innerHTML = ergebnis;
+}
 //# sourceMappingURL=main.js.map
--- aufgabe4/js/main.ts
+++ aufgabe4/js/main.ts
@@ -5,10 +5,10 @@ Matrikel: <260918>
 Datum: <18.04.2019>
 	
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
-
+let Bestellung: String[] = [];
 
 namespace Eisdealer {
-    
+
     window.addEventListener("load", init);
 
     function init(_event: Event): void {
@@ -18,23 +18,55 @@ namespace Eisdealer {
         for (let i: number = 0; i < fieldsets.length; i++) {
             let fieldset: HTMLFieldSetElement = fieldsets[i];
             fieldset.addEventListener("change", bestellen);
+            fieldset.addEventListener("change", berechnePreis);
         }
     }
 
-
-
     function bestellen(_event: Event): void {
-        
+
         let target: HTMLInputElement = <HTMLInputElement>_event.target;
         //*/ note: this == _event.currentTarget in an event-handler
-        if (this.id == "checkbox" || this.id == "number") 
+        if (this.id == "checkbox" || this.id == "number")
             console.log("Changed " + target.name + " to " + target.checked);
 
-            let prodElement = document.createElement('div');
-            document.getElementById("bestellung").appendChild(prodElement);
-            let gewählt: string = `
-            <p id="${target.id}">${target.id}</p>
-            `
-             prodElement.innerHTML = gewählt;
+        let prodElement = document.createElement('div');
+        document.getElementById("bestellung").appendChild(prodElement);
+
+        Bestellung.push(target.value);
+
+        let gewählt: string = `
+            <p id="${target.id}">${Bestellung}</p>`
+        prodElement.innerHTML = gewählt;
     }
- }
+}
+
+let Eis: string[] = [];
+let Zusatz: string[] = [];
+
+function berechnePreis(_event: Event) {
+    let currentEventtarget: HTMLInputElement = <HTMLInputElement>_event.currentTarget;
+    let target: HTMLInputElement = <HTMLInputElement>_event.target;
+    if (currentEventtarget.id == "eissorten") {
+        /*alert("HiEissorte");*/
+        Eis.push(target.value);
+        console.log(Eis.length * 1);
+    }
+    else if (currentEventtarget.id == "zusätze") {
+        /*alert("hizusatz")*/
+        Zusatz.push(target.value);
+        console.log(Zusatz.length * 0.5);
+    };
+
+    let Ergebnis: number = Eis.length * 1;
+    let Ergebnis2: number = Zusatz.length * 0.5;
+
+    let prodElement = document.createElement('div');
+    document.getElementById("bestellung").appendChild(prodElement);
+
+    let ergebnis: string = `
+            <p id="${target.id}">${Ergebnis + Ergebnis2}</p>`
+    prodElement.innerHTML = ergebnis;
+}
+
+
+
